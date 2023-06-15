460f011b53c2577b74bcdbaffa871a1e6a3ab9ae
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-06-15 00:36:19 -0300

    Creating form component

---
+++ src/components/Agents/AgentForm.tsx
@@ -0,0 +1,18 @@
+import type { FC } from "react";
+// import { IAgent } from "../../types/Agent";
+
+const AgentForm: FC<{}> = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default AgentForm;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,13 @@ import type { FC } from "react";
 import "./App.css";
 
 import Agents from "../Agents/Agents";
+import AgentForm from "../Agents/AgentForm";
 
 const App: FC = () => {
   return (
     <div className="app">
       <Agents />
+      <AgentForm />
     </div>
   );
 };
