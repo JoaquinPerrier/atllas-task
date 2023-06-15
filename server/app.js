79801b5d5cc0c150ccd0baadde6e0f9f924c0b36
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-06-15 12:10:42 -0300

    Receiving params in the backend

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const { Agent } = require('./model');
+const express = require("express");
+const { Agent } = require("./model");
 
 const app = express();
 
-app.get('/agents', async (req, res, next) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/agents", async (req, res, next) => {
   const agents = await Agent.findAll();
   return res.json(agents);
+});
+
+app.post("/agents", async (req, res) => {
+  try {
+    //let { firstName, lastName } = req.body;
+    console.log(req.body);
+    return "Ready pal ranked";
+  } catch (err) {
+    res.status(401).send({ message: err.message });
+  }
 });
 
 module.exports = app;
